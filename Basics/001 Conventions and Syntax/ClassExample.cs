32cb81011b161bc32a07563b206d9f408adba8bd
Aeryn Peterson <155934906+aerynp522@users.noreply.github.com>
2024-01-14 12:45:59 -0700

    i changed to crustydog

--- Basics/001 Conventions and Syntax/ClassExample.cs
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -1,21 +1,25 @@
 //Use this file to store your work to demonstrate your understanding of operators
 //Make sure your work is different from the instructors examples or your peers
 
-public class ClassExample
-{
+public class CrunchyDog
+{ 
+    public DogFeatures crustyWhiteDog;
+    
+    public void RunningDog
+    {
+        crustyWhiteDog = new DogFeatures();
+
+        Console.WriteLine(crustyWhiteDog.doggoName + "has" + crustyWhiteDog.numEyes + "eyes with a laser power of" + crustyWhiteDog.laserPower);
+        Console.WriteLine(crustyWhiteDog.doggoName + "has" + crustyWhiteDog.numLasers + "lasers.")
+    }
     
 }
 
-public class ClassWithVariables
+public class DogFeatures
 {
-    public int number = 2;
-    public string userName = "Bob";
+    public int numLasers = 15;
+    public int numEyes = 2;
+    public float laserPower = 20.6f;
+    public string doggoName = "Cronch";
 }
 
-public class ClassWithFunctions
-{
-    public void RunFunction()
-    {
-        Console.WriteLine("Running Function");
-    }
-}   
