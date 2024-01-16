d27664d43950602ee28ad3f112f80ec12cfd5565
Aeryn Peterson <155934906+aerynp522@users.noreply.github.com>
2024-01-15 17:31:16 -0700

    crunchydog

--- Basics/001 Conventions and Syntax/Weapons.cs
+++ Basics/001 Conventions and Syntax/Weapons.cs
@@ -1,28 +1,23 @@
 using System;
-					
-public class Program
+
+public class CrunchyDog
+{ 
+    public DogFeatures crustyWhiteDog;
+    
+    public void Main()
+    {
+        crustyWhiteDog = new DogFeatures();
+
+        Console.WriteLine(crustyWhiteDog.doggoName + " has " + crustyWhiteDog.numEyes + " eyes with a laser power of " + crustyWhiteDog.laserPower);
+        Console.WriteLine(crustyWhiteDog.doggoName + " has " + crustyWhiteDog.numLasers + " lasers all over his body.");
+    }
+    
+}
+
+public class DogFeatures
 {
-	public Weapon miniGun;
-	public PowerUp weaponPowerUp;
-	
-	public void Main()
-	{
-		miniGun = new Weapon();
-		weaponPowerUp = new PowerUp();
-		
-		miniGun.ammoCount++;
-		miniGun.firePower += weaponPowerUp.powerLevel;
-		Console.WriteLine(miniGun.weaponName + " has an ammo count of " + miniGun.ammoCount + ".");
-		Console.WriteLine("The fire power of is now " + miniGun.firePower + ", it has increased.");
-	}
-}
-
-public class Weapon { 
-	public int ammoCount = 10;
-	public string weaponName = "Mini Gun";
-	public float firePower = 11.5f;
-}
-
-public class PowerUp {
-	public float powerLevel = 2.5f;	
+    public int numLasers = 15;
+    public int numEyes = 2;
+    public float laserPower = 20.6f;
+    public string doggoName = "Cronch";
 }
